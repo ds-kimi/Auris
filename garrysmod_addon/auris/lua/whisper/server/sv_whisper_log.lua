cc6917875dbc7ed21b89eb9b48a5d65487bec3db
Vitroze <azouaouiaziz4@gmail.com>
2026-04-07 13:21:28 +0200

    If debug mode is not enabled, then the functions are not loaded.

--- garrysmod_addon/auris/lua/whisper/server/sv_whisper_log.lua
+++ garrysmod_addon/auris/lua/whisper/server/sv_whisper_log.lua
@@ -1,4 +1,6 @@
 -- Poll whisper for transcriptions and log them
+if not whisper.IsDebug() then return end
+
 local function formatTimestamp()
     return os.date("[%H:%M:%S]")
 end
@@ -12,16 +14,14 @@ local function writeTranscript(sid, text)
     print("[Whisper] " .. name .. ": " .. text)
 end
 
-if whisper.IsDebug() then
-    timer.Create("Whisper_Poll", 0.1, 0, function()
-        if not whisper.IsDebug() then return end
+timer.Create("Whisper_Poll", 0.1, 0, function()
+    if not whisper.IsDebug() then return end
 
-        local sid, text = whisper.Poll()
-        while sid do
-            writeTranscript(sid, text)
-            sid, text = whisper.Poll()
-        end
-    end)
-end
+    local sid, text = whisper.Poll()
+    while sid do
+        writeTranscript(sid, text)
+        sid, text = whisper.Poll()
+    end
+end)
 
 whisper.Debug(true)
